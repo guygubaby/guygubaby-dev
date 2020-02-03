7c2a57b10c22d3d4c45f52aac9502d4e865c85b2
yinjiangbo <1907004005@qq.com>
2020-02-03 15:12:44 +0800

    init

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="LandingPage bg-purple-400">
+
+    <div class="hero bg-purple-700 pt-24">
+      <div class="container mx-auto justify-center items-center mt-32">
+        
+        <div class="container text-white text-center">
+
+          <div class="text-5xl font-bold">Hi, I'm <span class="hover:text-purple-300">guygubaby</span>.</div>
+          <div class="text-2xl font-normal text-gray-300">I wrote js and python</div>
+
+          <!-- <div class="flex-auto items-center justify-between flex-wrap mt-4 pb-12 px-4">
+
+            <a href="https://discord.gg/3eHf5j2" class="shadow-xl bg-indigo-500 hover:bg-indigo-400 text-white font-bold py-2 px-4 rounded-full mr-3">
+              <i class="fab fa-discord mr-2"></i>Contact
+            </a>
+
+            <a href="https://top.gg/user/204616460797083648" class="shadow-xl bg-indigo-500 hover:bg-indigo-400 text-white font-bold py-2 px-4 rounded-full">
+              <i class="fas fa-meteor mr-2"></i>Bots
+            </a>
+
+          </div> -->
+        </div>
+
+      </div>
+    </div>
+    
+    <div class="wave-container">
+      <svg class="fill-current text-purple-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 70 1440 320">
+        <path d="M0,288L48,266.7C96,245,192,203,288,202.7C384,203,480,245,576,266.7C672,288,768,288,864,261.3C960,235,1056,181,1152,149.3C1248,117,1344,107,1392,101.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+      </svg>
+    </div>
+
+    <div class="hero pb-32 mx-8 my-8 pt-20">
+      <div class="container text-black text-left mx-auto font-sans">
+
+        <kinesis-container>
+
+          <kinesis-element type="depth_inv" :strength="5">
+        
+            <div class="container about text-gray-200 bg-purple-700 rounded-lg p-8 shadow-lg" v-scroll-reveal="{ delay: 150 }">
+              <div class="text-4xl font-bold"><i class="fas fa-info-circle mr-3"></i>About Me</div>
+
+              <hr>
+
+              <div class="text-xl font-normal mt-2">I'm a <span class="font-bold hover:text-purple-300">23 year old</span> web developer from the <span class="font-bold hover:text-purple-300">China</span> who enjoys working with both <i class="fab fa-js mr-2"></i><a class="font-bold underline hover:text-purple-300" href="https://www.ecma-international.org/memento/tc39.htm">Javascript</a> &amp; <i class="fab fa-python mr-2"></i><a class="font-bold underline hover:text-purple-300" href="https://python.org/">Python</a>.</div>
+            </div>
+          
+          </kinesis-element>
+        
+        </kinesis-container>
+
+        <kinesis-container>
+
+          <kinesis-element type="depth_inv" :strength="5">
+
+            <div class="container skills text-gray-200 bg-purple-700 rounded-lg p-8 mt-8 shadow-lg" v-scroll-reveal="{ delay: 150 }">
+              <div class="text-4xl font-bold"><i class="fas fa-bookmark mr-3"></i>Skills</div>
+
+              <hr>
+
+              <div class="text-xl font-normal mt-2">
+                <ul class="list-disc ml-4">
+                  <li><i class="fas fa-database mr-2"></i>MongoDB, Redis</li>
+                  <li><i class="fab fa-vuejs mr-2"></i>Vue.js</li>
+                  <li><i class="fab fa-python mr-2"></i>Python</li>
+                  <li><i class="fas fa-server mr-2"></i>Nginx</li>
+                  <li><i class="fab fa-github mr-2"></i>Git</li>
+                </ul>
+              </div>
+            </div>
+          
+          </kinesis-element>
+        
+        </kinesis-container>
+
+        <kinesis-container>
+
+          <kinesis-element type="scale" :strength="1">
+
+            <router-link to="projects">
+              <div class="container projects text-gray-200 bg-purple-700 rounded-lg p-8 mt-8 shadow-lg text-center hover:bg-purple-600" v-scroll-reveal="{ delay: 150 }">
+                <div class="text-4xl font-bold"><i class="fas fa-arrow-right mr-3"></i>View Projects</div>
+              </div>
+            </router-link>
+          
+          </kinesis-element>
+        
+        </kinesis-container>
+
+      </div>
+    </div>
+
+    <div class="wave-container">
+      <svg class="fill-current text-purple-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path d="M0,224L48,229.3C96,235,192,245,288,256C384,267,480,277,576,261.3C672,245,768,203,864,186.7C960,171,1056,181,1152,208C1248,235,1344,277,1392,298.7L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+      </svg>
+    </div>
+
+    <div class="hero-footer bg-purple-700 p-12 text-white">
+      <div class="container flex items-center justify-center mx-auto text-center font-bold">
+        <span class="rounded-full bg-purple-800 px-4 py-2 shadow-lg m-1">
+          <i class="fas fa-code mr-3"></i>guygubaby Development &copy; 2020
+        </span>
+
+        <a href="mailto:1907004005@qq.com" class="rounded-full bg-purple-800 px-4 py-2 shadow-lg m-1 font-bold">
+          <i class="fas fa-envelope mr-2"></i>1907004005@qq.com
+        </a>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+hr {
+    border: 0;
+    height: 2px;
+    margin-top: 5px;
+    margin-bottom: 15px;
+    background-color: white;
+}
+</style>
---
+++ src/views/ProjectsPage.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="ProjectsPage">
+        <div class="hero h-screen bg-purple-700 pt-24 pb-12">
+            <div class="container mx-auto p-6">
+                <div class="text-white text-5xl font-bold"><i class="fas fa-leaf mr-3"></i>Projects</div>
+                <div class="text-gray-300 text-2xl font-medium">A collection of active projects.</div>
+
+                <hr class="my-8">
+                
+                <section class="text-white text-center">
+                    <carousel :loop="true" :autoplay="true" :perPage="2" paginationActiveColor="#5D028B" paginationColor="#FFFFFF">
+                        <slide v-for="project in projects" v-bind:key="project.id">
+                            <div class="container px-2">
+                                <a :href="project.url">
+                                    <div class="info container bg-purple-800 py-16 px-4 rounded-lg shadow-lg mb-8 text-wrap hover:bg-purple-900">
+                                        <div class="font-bold text-2xl"><BaseIcon :name="project.icon" />{{ project.name }}</div>
+                                        <div class="font-medium text-gray-300 text-lg">{{ project.description }}</div>
+                                    </div>
+                                </a>
+                            </div>
+                        </slide>
+                    </carousel>
+                    
+                </section>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import BaseIcon from '@/components/BaseIcon';
+
+export default {
+    name: 'ProjectsPage',
+    components: {
+        BaseIcon
+    },
+    data: function () {
+        return {
+            projects: [
+                {
+                    id: 1,
+                    name: 'hot-list-server',
+                    description: 'hot list server side wrote in python',
+                    url: 'https://github.com/guygubaby/hot-list',
+                    icon: 'fas fa-pen-fancy',
+                },
+                {
+                    id: 2,
+                    name: 'Blog',
+                    description: 'My skills blog',
+                    url: 'https://github.com/guygubaby/blog',
+                    icon: 'fab fa-discord',
+                },
+                {
+                    id: 3,
+                    name: 'dytt-server',
+                    description: 'A spider wrote in golang',
+                    url: 'https://github.com/dytt-bryce/server',
+                    icon: 'fab fa-discord',
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+hr {
+    border: 0;
+    height: 2px;
+    background-color: white;
+}
+</style>
